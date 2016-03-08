5b511d215a69ff50daaa00509a2178a6e719d3f9
rpham64 <rpham64@gmail.com>
2016-03-08 03:27:27 -0800

    First Commit: Started Chapter 18.

--- java/com/bignerdranch/android/beatbox/BeatBoxActivity.java
+++ java/com/bignerdranch/android/beatbox/BeatBoxActivity.java
@@ -28,6 +28,8 @@ public class BeatBoxActivity extends AppCompatActivity {
         });
     }
 
+    //test
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
